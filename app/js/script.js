54a5965880b702cebc546bdbf56d0cf93105f885
AKD47 <van83horn@gmail.com>
2017-12-15 17:36:33 +0300

    top form mobile

--- app/js/script.js
+++ app/js/script.js
@@ -4,19 +4,43 @@ $(document).ready(function () {
     $(document).on('click', '#mobile-menu', function (event) {
         event.preventDefault();
         var menu = $(this).next('.header__nav');
-        $(this).toggleClass('js-trigger-active');
-        menu.slideToggle('slow');
+        if($(this).hasClass('js-trigger-active')){
+            $(this).removeClass('js-trigger-active');
+            menu.slideUp('slow');
+        } else {
+            $(this).addClass('js-trigger-active');
+            menu.slideDown('slow');
+        }
         return false;
     });
-    if(window.innerWidth < 756){
-        $(document).on('click', function (e) {
-            if ($(e.target).closest('.header__navigation').length != 1) {
-                $('.header__nav').slideUp('500');
+    if (window.innerWidth < 756) {
+        $(document).mouseup(function (event){ 
+            var block = $('.header__navigation'); 
+            if (!block.is(event.target) 
+                && block.has(event.target).length === 0) { 
                 $('#mobile-menu').removeClass('js-trigger-active');
+                $('.header__nav').slideUp('500');
             }
-        });
+        });        
     }
-   
+    /*close*/
+
+    /*header form*/
+    $(document).on('click', '.header__toolbar--user', function (event) {
+        event.preventDefault();
+        var formBox = $(this).next('.header__form-box');
+        $(this).toggleClass('js-form-active');
+        formBox.slideToggle('slow');
+        return false;
+    });
+    $(document).mouseup(function (event){ 
+        var block = $('.header__toolbar');
+        if (!block.is(event.target) 
+            && block.has(event.target).length === 0) { 
+            $('.header__toolbar--user').removeClass('js-form-active');
+            $('.header__form-box').slideUp('500'); 
+        }
+    });   
     /*close*/
 
     /*gallerylider*/
@@ -28,11 +52,11 @@ $(document).ready(function () {
 
     /*show faq acardeon*/
     $(document).on('click', '.faq__wrapper--trigger', function (event) {
-       event.preventDefault();
-       var trigger = $(this).find('.faq__wrapper--icon'),
-           text = $(this).next('.faq__wrapper--box'),
-           wrapper = $(this).closest('.faq__wrapper--element');
-        if($(this).hasClass('js-faq-show')){
+        event.preventDefault();
+        var trigger = $(this).find('.faq__wrapper--icon'),
+            text = $(this).next('.faq__wrapper--box'),
+            wrapper = $(this).closest('.faq__wrapper--element');
+        if ($(this).hasClass('js-faq-show')) {
             $(this).removeClass('js-faq-show');
             wrapper.removeClass('js-faq-wrapper');
             trigger.removeClass('js-faq-icon');
@@ -41,7 +65,7 @@ $(document).ready(function () {
             $(this).addClass('js-faq-show');
             wrapper.addClass('js-faq-wrapper');
             trigger.addClass('js-faq-icon');
-            text.slideDown('600');            
+            text.slideDown('600');
         }
     });
     /*close*/
@@ -50,7 +74,7 @@ $(document).ready(function () {
 
 /*gallery slider*/
 function gallerySlider() {
-    if($(document).width() < 770){
+    if ($(document).width() < 770) {
         $('#gallery-slider').slick({
             infinite: true,
             dots: true,
@@ -74,7 +98,7 @@ function gallerySlider() {
 
 /*gallery slider*/
 function cardsSlider() {
-    if($(document).width() < 770){
+    if ($(document).width() < 770) {
         $('#cards-slider').slick({
             infinite: true,
             dots: true,
