0024517489912f9964f0900c781ebed1d43bdff9
AKD47 <van83horn@gmail.com>
2017-12-26 14:04:21 +0300

    footer for ios

--- public/js/script.min.js
+++ public/js/script.min.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
 
     /*mobile menu*/
+    // if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+    //     $(document).on('click', '#mobile-menu', function (event) {
+    //         event.preventDefault();
+    //         var menu = $(this).next('.header__nav');
+    //         if ($(this).hasClass('js-trigger-active')) {
+    //             $(this).removeClass('js-trigger-active');
+    //             menu.slideUp('slow');
+    //         } else {
+    //             $(this).addClass('js-trigger-active');
+    //             menu.slideDown('slow');
+    //         }
+    //         return false;
+    //     });
+    //     if (window.innerWidth < 756) {
+    //         $(document).mouseup(function (event) {
+    //             var block = $('.header__navigation');
+    //             if (!block.is(event.target)
+    //                 && block.has(event.target).length === 0) {
+    //                 $('#mobile-menu').removeClass('js-trigger-active');
+    //                 $('.header__nav').slideUp('500');
+    //             }
+    //         });
+    //     }
+    // }
+
     $(document).on('click', '#mobile-menu', function (event) {
         event.preventDefault();
         var menu = $(this).next('.header__nav');
@@ -60,6 +85,7 @@ $(document).ready(function () {
 
     /*switch language*/
     $(document).on('click', '.header__lang--trigger', function (event) {
+        event.preventDefault();
         var list = $(this).next('.header__lang--list');
         list.slideToggle('500');
     });
@@ -113,10 +139,10 @@ $(document).ready(function () {
     /*close*/
 
     /*gallery slider*/
-    $(window).on('load resize', function () {
-        gallerySlider();
-        cardsSlider()
-    });
+    // $(window).on('load resize', function () {
+    //     gallerySlider();
+    //     cardsSlider()
+    // });
     /*close*/
 
     /*show partners form*/
